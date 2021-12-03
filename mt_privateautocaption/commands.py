17efd6863181410ca4595585fa6cd6d4b7698549
BotsDevs123 <93205572+BotsDevs123@users.noreply.github.com>
2021-12-03 14:14:52 +0530

    Update commands.py

--- mt_privateautocaption/commands.py
+++ mt_privateautocaption/commands.py
@@ -13,8 +13,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQ
 @Client.on_message(filters.private & filters.command("start"))
 async def start_meg(client, update):
     text = f"""<b> 👋Hello {update.from_user.mention}\n\nI am an AutoCaption bot\n\nAll you have to do is add me to your channel and I will show you my power\n\nFor more info check help Button\n\n</b>"""
-
     await update.reply_text(
-        text=text,
+        text=text
   )
 
